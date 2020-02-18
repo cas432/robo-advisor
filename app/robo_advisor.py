ef7264ffac0063fb1584b7a0099f5f7eee7b3751
cas432 <cas432@georgetown.edu>
2020-02-18 10:02:21 -0500

    import requests/json

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,3 +1,29 @@
+
+import requests
+import json
+
+#INFO INPUTS
+
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
+response = requests.get(request_url)
+#print(type(response)) #> <class 'requests.models.Response'>
+#print(response.status_code) #>200
+#print(response.text)
+
+parsed_response = json.loads(response.text)
+
+
+
+
+
+breakpoint()
+
+
+
+
+
+
+
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
