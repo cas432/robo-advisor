fb4817324bb6dae6dd44a50de344c3979b429a05
cas432 <cas432@georgetown.edu>
2020-02-23 14:49:35 -0500

    fixed errors

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -17,97 +17,110 @@ def to_usd(my_price):
 symbols = []
 
 
+
 while True: 
     symbol = input("Please input your stock ticker symbol of choice:")
          
     
     if symbol == "DONE":
+        if len(symbols) == 0:
+            print("\n    Please enter at least one stock symbol")
+        else:
             break
+
     elif float(len(symbol)) <= 5 and symbol.isalpha():
         symbols.append(symbol)
+        
     else:
         print("\n    Please enter a valid symbol or type 'DONE' to finish adding stocks. \n")
 
-            
 
-#must be letters, multiple at once
-#symbol = "MSFT" #TODO ACCEPT USER INPUT
-
-#
-api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
-request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
+for s in symbols:
 
 
-response = requests.get(request_url)
-#print(type(response)) #> <class 'requests.models.Response'>
-#print(response.status_code) #>200
-#print(response.text)
-
-parsed_response = json.loads(response.text)
-
-last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
-
-tsd = parsed_response["Time Series (Daily)"]
-
-dates = list(tsd.keys()) #TODO ASSUME LATEST DAT IS FIRST
-latest_day = dates[0]
-
-latest_close = tsd[latest_day]["4. close"]
+    api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={s}&apikey={api_key}"
 
 
+    response = requests.get(request_url)
+
+    if "Error Message" in response.text:
+        print("OOOPS")
+        break
+    else:
+
+        parsed_response = json.loads(response.text)
+
+        
+
+        tsd = parsed_response["Time Series (Daily)"]
+
+    dates = list(tsd.keys()) #TODO ASSUME LATEST DAY IS FIRST. sort chronologically?
+   
+    latest_day = dates[0]
+    last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]  
+    latest_close = tsd[latest_day]["4. close"]
+    
 
 
-#get high price from each day
-high_prices = []
 
-low_prices = []
-#recent_high = max(high_prices)
-for date in dates:
-    high_price = tsd[date]["2. high"]
-    high_prices.append(float(high_price))
-
-    low_price = tsd[date]["3. low"]
-    low_prices.append(float(low_price))
-recent_high = max(high_prices)
-recent_low = min(low_prices)
-
-#csv_file_path = "data/prices.csv" # a relative filepath
-csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
-
-csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
-
-with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
-    writer = csv.DictWriter(csv_file, fieldnames=csv_headers, lineterminator = '\n')
-    writer.writeheader() # uses fieldnames set above
+    #get high price from each day
+    high_prices = []
+    low_prices = []
+    #recent_high = max(high_prices)
     for date in dates:
-        daily_prices = tsd[date]
-             
-        writer.writerow({
-            "timestamp": date,
-            "open": daily_prices["1. open"],
-            "high": daily_prices["2. high"],
-            "low": daily_prices["3. low"],
-            "close": daily_prices["4. close"],
-            "volume": daily_prices["5. volume"]
-        })
+        high_price = tsd[date]["2. high"]
+        high_prices.append(float(high_price))
 
-print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
-print("-------------------------")
-print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
-print("-------------------------")
-print("LATEST DAY: " + last_refreshed) #########
-print("LATEST CLOSE: " + to_usd(float(latest_close))) ##########
-print("RECENT HIGH: " + to_usd(float(recent_high)))
-print("RECENT LOW: " + to_usd(float(recent_low)))
-print("-------------------------")
-print("RECOMMENDATION: BUY!")
-print("RECOMMENDATION REASON: TODO")
-print("-------------------------")
-print("WRITING DATA TO CSV: " + csv_file_path)
-print("-------------------------")
-print("HAPPY INVESTING!")
-print("-------------------------")
+        low_price = tsd[date]["3. low"]
+        low_prices.append(float(low_price))
+    recent_high = max(high_prices)
+    recent_low = min(low_prices)
 
 
+
+    #csv_file_path = "data/prices.csv" # a relative filepath
+    csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices_" + s + ".csv")
+
+    csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
+
+    with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+        writer = csv.DictWriter(csv_file, fieldnames=csv_headers, lineterminator = '\n')
+        writer.writeheader() # uses fieldnames set above
+        for date in dates:
+            daily_prices = tsd[date]
+                
+            writer.writerow({
+                "timestamp": date,
+                "open": daily_prices["1. open"],
+                "high": daily_prices["2. high"],
+                "low": daily_prices["3. low"],
+                "close": daily_prices["4. close"],
+                "volume": daily_prices["5. volume"]
+            })
+
+    print("-------------------------")
+    print("SELECTED SYMBOL: XYZ")
+    print("-------------------------")
+    print("REQUESTING STOCK MARKET DATA...")
+    print("REQUEST AT: 2018-02-20 02:00pm")
+    print("-------------------------")
+    print("LATEST DAY: " + last_refreshed) #########
+    print("LATEST CLOSE: " + to_usd(float(latest_close))) ##########
+    print("RECENT HIGH: " + to_usd(float(recent_high)))
+    print("RECENT LOW: " + to_usd(float(recent_low)))
+    print("-------------------------")
+    print("RECOMMENDATION: BUY!")
+    print("RECOMMENDATION REASON: TODO")
+    print("-------------------------")
+    print("WRITING DATA TO CSV: " + csv_file_path)
+    print("-------------------------")
+    print("HAPPY INVESTING!")
+    print("-------------------------")
+
+
+
+#detect/handle response errors
+#if "Error Message" in response.text:
+     #   print("OOOPS")
+      #  exit()
