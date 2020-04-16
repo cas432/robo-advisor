2efd5afc129ba2464c5b8703d03863c89ea747f3
cas432 <cas432@georgetown.edu>
2020-04-15 23:06:19 -0400

    tests

--- test/robo_advisor_test.py
+++ test/robo_advisor_test.py
@@ -1,4 +1,4 @@
-from app.robo_advisor import to_usd, parse_response, calc_recent_high, calc_recent_low, calc_close_price, get_url, sms_math_lower, sms_math_upper,threshold_calc
+from app.robo_advisor import to_usd, parse_response, calc_recent_high, calc_recent_low, sms_math_lower, sms_math_upper,threshold_calc
 import json
 import pytest
 import os
@@ -9,13 +9,11 @@ def test_to_usd():
     assert result == "$1,223.60"
 
 
-
 def test_calc_recent_high():
     temp_list = []
     tsd = {'2020-04-15': {'1. open': '80.7900', '2. high': '80.9800', '3. low': '79.4000', '4. close': '79.7900', '5. volume': '2972689'}, '2020-04-14': {'1. open': '81.6000', '2. high': '82.1700', '3. low': '80.3400', '4. close': '82.0200', '5. volume': '3390797'}, '2020-04-13': {'1. open': '80.6000', '2. high': '81.1600', '3. low': '78.4200', '4. close': '79.5300', '5. volume': '2739490'}, '2020-04-09': {'1. open': '78.6300', '2. high': '82.6400', '3. low': '78.6300', '4. close': '81.5900', '5. volume': '5478970'}, '2020-04-08': {'1. open': '74.0800', '2. high': '78.9000', '3. low': '73.5800', '4. close': '78.2200', '5. volume': '3891794'}, '2020-04-07': {'1. open': '75.6300', '2. high': '76.2400', '3. low': '73.1501', '4. close': '73.4400', '5. volume': '4258840'}} 
     high = calc_recent_high(tsd,"2020-04-15",temp_list)
     assert high == 80.98
-
 
 
 def test_calc_recent_low():
@@ -26,7 +24,6 @@ def test_calc_recent_low():
 
 
 def test_parse_response():
-
     s = "FB"
     parsed_response = parse_response(s)
     assert isinstance(parsed_response, dict)
@@ -42,7 +39,6 @@ def test_sms_math_lower():
 def test_sms_math_upper():
     result = sms_math_upper(0.05,100)
     assert result == 105
-
 
 def test_threshold_calc():
     result = threshold_calc(0.10,1000)
